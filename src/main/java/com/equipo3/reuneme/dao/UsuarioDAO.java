840b9b90773353901d82b2dd8cdd304fdfacc07e
Guillermo <hola@swey.net>
2024-10-07 22:57:25 +0200

    Avance Tarea Procesar Registro en Backend

---
+++ src/main/java/com/equipo3/reuneme/dao/UsuarioDAO.java
@@ -0,0 +1,13 @@
+package com.equipo3.reuneme.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.equipo3.reuneme.model.Usuario;
+
+public interface UsuarioDAO extends JpaRepository<Usuario, String>{
+
+	Usuario findByEmailAndPwd(String email, String pwd);
+	Usuario findByEmail(String email);
+	
+}
+
---
+++ src/main/java/com/equipo3/reuneme/http/UsuarioController.java
@@ -0,0 +1,45 @@
+package com.equipo3.reuneme.http;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.equipo3.reuneme.model.DatosRegistro;
+import com.equipo3.reuneme.model.Usuario;
+import com.equipo3.reuneme.services.EmailService;
+import com.equipo3.reuneme.services.UsuarioService;
+
+
+@RestController
+@RequestMapping("users")
+@CrossOrigin("*")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService uService;
+
+	@Autowired
+	private EmailService emailService;
+	    
+	    @PostMapping("/register")
+	    public void register(@RequestBody DatosRegistro datos) {
+	    	
+	    	//Comprobamos que la contraseña cumple requisitos seguridad y ambas contraseñas son iguales
+	    	datos.comprobarPwd();
+	    	
+	    	//Comprobamos que el email tiene un formato válido
+	        if(emailService.validarEmail(datos.getEmail()) == false) {
+	        	throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "El email insertado no tiene un formato válido");
+	        }
+	        
+	        //Si pasa los controles, se registra en BD
+	        Usuario user = new Usuario(datos);
+	        this.uService.registrar(user);
+	    }
+
+}
