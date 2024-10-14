a9c95544195186187eede780f8b379ff1c56d08c
Guillermo <hola@swey.net>
2024-10-14 16:56:54 +0200

    Fix Login

--- src/main/java/com/equipo3/reuneme/Auditable.java
+++ src/main/java/com/equipo3/reuneme/Auditable.java
@@ -1,7 +1,11 @@
+package com.equipo3.reuneme;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
