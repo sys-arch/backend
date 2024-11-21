99fe35337ae6c7693ff875593c0061840d7c0c78
‎WM <hola@swey.net>
2024-11-21 15:38:53 +0100

    Cambio de Puerto Render

--- src/main/java/com/equipo3/reuneme/config/AppConfiguration.java
+++ src/main/java/com/equipo3/reuneme/config/AppConfiguration.java
@@ -10,10 +10,10 @@ public class AppConfiguration implements WebServerFactoryCustomizer<TomcatServle
     @Override
     public void customize(TomcatServletWebServerFactory factory) {
         factory.addConnectorCustomizers((connector) -> {
-            connector.setPort(8442);
+            connector.setPort(443);
             connector.setSecure(true);
             connector.setScheme("https");
-            connector.setRedirectPort(8442);
+            connector.setRedirectPort(443);
         });
     }
 }
